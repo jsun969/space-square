57c44e208051b08ea829e109b94e165e9e748be8
jsun969 <i@jsun.lol>
2025-12-18 00:18:18 +0800

    feat: parse cli args

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,34 @@
 #include "Scanner.hpp"
 #include "fmt/base.h"
+#include <cxxopts.hpp>
 #include <fmt/ranges.h>
 #include <functional>
 
-int main() {
+int main(int argc, char* argv[]) {
+	// Parse command line arguments
+	cxxopts::Options options("spsq", "View your space in squares");
+
+	options.add_options()("path", "Path to scan", cxxopts::value<std::string>()->default_value(".")) //
+			("d,depth", "Depth of directory traversal", cxxopts::value<int>()->default_value("3"))			 //
+			("h,help", "Print help information");
+
+	options.parse_positional({ "path" });
+	options.positional_help("[path]");
+
+	auto result = options.parse(argc, argv);
+
+	if (result.count("help")) {
+		fmt::println("{}", options.help());
+		return 0;
+	}
+
+	std::string rootPath = result["path"].as<std::string>();
+	int depth = result["depth"].as<int>();
+
+	// Scan the directory
 	spsq::Scanner scanner;
-	auto files = scanner.scan(".", 2);
+	auto files = scanner.scan(rootPath, depth);
+	// DEBUG
 	std::function<void(const spsq::File&, int)> printFile = [&](const spsq::File& file, int indent = 0) {
 		std::string indentStr(indent * 2, ' ');
 		auto fileTypeStr = file.type == spsq::FileType::Directory ? "Directory" : file.type == spsq::FileType::File ? "File"
@@ -16,5 +39,6 @@ int main() {
 		}
 	};
 	printFile(files, 0);
+	// DEBUG
 	return 0;
 }
